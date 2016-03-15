f2e679f0e5a7b69606a13fa883d5c65a65ee69f8
Ziyang tan <ziyang0621@gmail.com>
2016-03-15 09:03:44 -0700

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -59,6 +59,33 @@ app.delete('/todos/:id', function (req, res) {
   }
 });
 
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+  var todoID = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoID});
+  var body = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+
+  if (!matchedTodo) {
+    return res.status(404).send();
+  }
+
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
+    return res.status(400).send();
+  }
+
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+    validAttributes.description = body.description.trim();
+  } else if (body.hasOwnProperty('description')) {
+    return res.status(400).send();
+  }
+
+  _.extend(matchedTodo, validAttributes);
+  res.json(matchedTodo);
+});
+
 app.listen(PORT, function () {
   console.log('Express listening on port ' + PORT + '!');
 });
