e27384495e439b7237541a02c23cf5fdaddc8209
Ziyang tan <ziyang0621@gmail.com>
2016-03-24 17:37:05 -0700

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
-module.exports = function (sequelize, DataType) {
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
+module.exports = function(sequelize, DataType) {
 	return sequelize.define('user', {
 		email: {
 			type: DataType.STRING,
@@ -8,19 +11,39 @@ module.exports = function (sequelize, DataType) {
 				isEmail: true
 			}
 		},
+		salt: {
+			type: DataType.STRING
+		},
+		password_hash: {
+			type: DataType.STRING
+		},
 		password: {
-			type: DataType.STRING,
+			type: DataType.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [7, 100]
+			},
+			set: function(value) {
+				var salt = bcrypt.genSaltSync(10);
+				var hashedPassword = bcrypt.hashSync(value, salt);
+
+				this.setDataValue('password', value);
+				this.setDataValue('salt', salt);
+				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
 	}, {
 		hooks: {
-			beforeValidate: function (user, options) {
+			beforeValidate: function(user, options) {
 				if (typeof user.email == 'string') {
 					user.email = user.email.toLowerCase();
 				}
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			}
 		}
 	});
